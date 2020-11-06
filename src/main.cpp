318d11f55e5247e3b876b00417d68ac46841790d
simjus <73905423+simjus@users.noreply.github.com>
2020-11-06 15:00:51 +0100

    Visual Studio Code support

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World!";
+    cout << "Hello World!" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << i << endl;
+    }
     return 0;
 }
