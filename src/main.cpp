d6dea70209d38088dedf06b9352b7237d19dd3db
simjus <73905423+simjus@users.noreply.github.com>
2021-02-19 11:49:59 +0100

    Some Editing

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    cout << "Start of the programm." << endl;
     cout << "Hello World!" << endl;
     for (int i = 0; i < 10; i++) {
         cout << i << endl;
