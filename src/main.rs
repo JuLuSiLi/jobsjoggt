bd1ca92c2d20ae7094076c9919f1c4c36b478514
Luca Z <l-zeuch@email.de>
2021-03-13 14:03:51 +0100

    test(main): add unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, jobsjoggt!");
+    let a = 5;
+    let b = 12;
+    println!("Hello, jobsjoggt! Your result is: {}", add(a, b));
 }
+
+fn add(a: i32, b: i32) -> i32 {
+    return a + b;
+}
+
+#[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+    }
