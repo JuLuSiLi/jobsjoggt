b8b12706678106fa52031e4dcd6cdb390a417579
simjus <73905423+simjus@users.noreply.github.com>
2021-03-16 18:23:31 +0100

    Transform supports coordinate system convertion

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ pub mod transform;
 use transform::Transform;
 
 fn main() {
-    let t = Transform::new();
-    println!("{:?}", t);
+    let mut t = Transform::new();
+    t.set_position(Vector3::new(2.0, -5.0, 3.0));
+    t.set_rotation(Vector3::new(0.0, 0.0, 90.0));
+    t.set_scale(Vector3::new(2.0, 1.0, 1.0));
+
+    let p = Vector3::new(1.0, 0.0, 0.0);
+
+    println!("{:?}", t.transform_point(p));
 }
--- src/transform.rs
+++ src/transform.rs
@@ -2,9 +2,14 @@ use crate::vector_math::*;
 
 #[derive(Debug)]
 pub struct Transform {
-    pub position: Vector3,
-    pub rotation: Vector3,
-    pub scale: Vector3,
+    position: Vector3,
+    rotation: Vector3, // In Degrees
+    scale: Vector3,
+
+    has_changed: bool,
+
+    local_to_world: Matrix4x4,
+    world_to_local: Matrix4x4,
 }
 
 impl Transform {
@@ -13,6 +18,74 @@ impl Transform {
             position: Vector3::new(0.0, 0.0, 0.0),
             rotation: Vector3::new(0.0, 0.0, 0.0),
             scale: Vector3::new(1.0, 1.0, 1.0),
+
+            has_changed: false,
+
+            local_to_world: Matrix4x4::identity(),
+            world_to_local: Matrix4x4::identity(),
         }
+    }
+    
+    pub fn set_position(&mut self, position: Vector3) {
+        self.position = position;
+        self.has_changed = true;
+    }
+
+    pub fn set_rotation(&mut self, rotation: Vector3) {
+        self.rotation = rotation;
+        self.has_changed = true;
+    }
+
+    pub fn set_scale(&mut self, scale: Vector3) {
+        self.scale = scale;
+        self.has_changed = true;
+    }
+
+    pub fn get_position(&self) -> Vector3 {
+        self.position
+    }
+
+    pub fn get_rotation(&self) -> Vector3 {
+        self.rotation
+    }
+
+    pub fn get_scale(&self) -> Vector3 {
+        self.scale
+    }
+
+    pub fn transform_point(&mut self, point: Vector3) -> Vector3 {
+        if self.has_changed {
+            self.compute_matrices();
+        }
+        
+        self.local_to_world * point
+    }
+
+    pub fn inv_transform_point(&mut self, point: Vector3) -> Vector3 {
+        if self.has_changed {
+            self.compute_matrices();
+        }
+        
+        self.world_to_local * point
+    }
+
+    // Apply order:
+    // Scale
+    // Rotation: Z-X-Y
+    // Translation
+    fn compute_matrices(&mut self) {
+        self.local_to_world =
+            Matrix4x4::translation(self.position) *
+            (Matrix4x4::y_rotation(self.rotation.y.to_radians()) *
+            (Matrix4x4::x_rotation(self.rotation.x.to_radians()) *
+            (Matrix4x4::z_rotation(self.rotation.z.to_radians()) *
+            Matrix4x4::scale(self.scale))));
+        
+        self.world_to_local =
+            Matrix4x4::scale(self.scale) *
+            (Matrix4x4::z_rotation(self.rotation.z.to_radians()) *
+            (Matrix4x4::x_rotation(self.rotation.x.to_radians()) *
+            (Matrix4x4::y_rotation(self.rotation.y.to_radians()) *
+            Matrix4x4::translation(self.position))));
     }
 }
