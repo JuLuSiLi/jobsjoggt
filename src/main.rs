c4fdec7cfbbc5dd53943e0c8dc27ddd5371c171d
Goldscar1 <yass@mein.gmx>
2021-03-17 14:49:45 +0100

    modified main.rs

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ fn main() {
                 *control_flow = ControlFlow::Exit
             },
             Event::MainEventsCleared => {
+                println!("This should loop");
                 // Application update code.
     
                 // Queue a RedrawRequested event.
@@ -46,18 +47,4 @@ fn main() {
             _ => ()
         }
     });
-  
-
-    /*let a = 5;
-    let b = 12;
-    println!("Hello, jobsjoggt! Your result is: {}", add(a, b));*/
 }
-
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-#[test]
-    fn test_add() {
-        assert_eq!(add(2, 3), 5);
-    }
